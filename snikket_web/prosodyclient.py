snikket-im/snikket-web-portal

commit e476d9b7c246581d46c33523e1c80beb09491137

Implement admin dashboard
Fixes #23.

snikket_web/prosodyclient.py
@@ -1,9 +1,13 @@
+import dataclasses
+import enum
 import functools
 import hashlib
 import logging
 import secrets
 import types
 import typing
+
+from datetime import datetime
 
 import aiohttp
 
@@ -19,7 +23,69 @@ from . import xmpputil
 from .xmpputil import split_jid
 
 
+SCOPE_DEFAULT = "prosody:scope:default"
+SCOPE_ADMIN = "prosody:scope:admin"
+
+
 T = typing.TypeVar("T")
+
+
+@dataclasses.dataclass(frozen=True)
+class TokenInfo:
+    token: str
+    scopes: typing.Collection[str]
+
+
+@dataclasses.dataclass(frozen=True)
+class AdminUserInfo:
+    localpart: str
+    display_name: typing.Optional[str]
+    email: typing.Optional[str]
+    phone: typing.Optional[str]
+
+    @classmethod
+    def from_api_response(
+            cls,
+            data: typing.Mapping[str, typing.Any],
+            ) -> "AdminUserInfo":
+        return cls(
+            localpart=data["username"],
+            display_name=data.get("display_name") or None,
+            email=data.get("email") or None,
+            phone=data.get("phone") or None,
+        )
+
+
+class InviteType(enum.Enum):
+    REGISTER = "register"
+
+
+@dataclasses.dataclass(frozen=True)
+class AdminInviteInfo:
+    id_: str
+    type_: InviteType
+    jid: typing.Optional[str]
+    token: str
+    xmpp_uri: typing.Optional[str]
+    landing_page: typing.Optional[str]
+    created_at: datetime
+    expires: datetime
+
+    @classmethod
+    def from_api_response(
+            cls,
+            data: typing.Mapping[str, typing.Any],
+            ) -> "AdminInviteInfo":
+        return cls(
+            id_=data["id"],
+            created_at=datetime.utcfromtimestamp(data["created_at"]),
+            expires=datetime.utcfromtimestamp(data["expires"]),
+            type_=InviteType(data["type"]),
+            jid=data["jid"],
+            token=data["id"],
+            xmpp_uri=data.get("xmpp_uri"),
+            landing_page=data.get("landing_page"),
+        )
 
 
 class HTTPSessionManager:
@@ -119,6 +185,7 @@ class ProsodyClient:
     CTX_AUTH_SESSION = "_ProsodyClient__auth_session"
     CONFIG_ENDPOINT = "PROSODY_ENDPOINT"
     SESSION_TOKEN = "prosody_access_token"
+    SESSION_CACHED_SCOPE = "prosody_scope_cache"
     SESSION_ADDRESS = "prosody_jid"
 
     def __init__(self, app: typing.Optional[quart.Quart] = None):
@@ -158,19 +225,26 @@ class ProsodyClient:
     def _rest_endpoint(self) -> str:
         return "{}/rest".format(self._endpoint_base)
 
+    def _admin_v1_endpoint(self, subpath: str) -> str:
+        return "{}/admin_api{}".format(self._endpoint_base, subpath)
+
     async def _oauth2_bearer_token(self,
                                    session: aiohttp.ClientSession,
                                    jid: str,
-                                   password: str) -> None:
+                                   password: str) -> TokenInfo:
         request = aiohttp.FormData()
         request.add_field("grant_type", "password")
         request.add_field("username", jid)
         request.add_field("password", password)
+        request.add_field(
+            "scope",
+            " ".join([SCOPE_DEFAULT, SCOPE_ADMIN])
+        )
 
         self.logger.debug("sending OAuth2 request (payload omitted)")
         async with session.post(self._login_endpoint, data=request) as resp:
             auth_status = resp.status
-            auth_info = (await resp.json())
+            auth_info: typing.Mapping[str, str] = (await resp.json())
 
             if auth_status in [400, 401]:
                 self.logger.debug("oauth2 error: %r", auth_info)
@@ -189,7 +263,10 @@ class ProsodyClient:
                             auth_info["token_type"]
                         )
                     )
-                return auth_info["access_token"]
+                return TokenInfo(
+                    token=auth_info["access_token"],
+                    scopes=auth_info["scope"].split(),
+                )
 
             raise RuntimeError(
                 "unexpected authentication reply: ({}) {!r}".format(
@@ -199,10 +276,13 @@ class ProsodyClient:
 
     async def login(self, jid: str, password: str) -> bool:
         async with self._plain_session as session:
-            token = await self._oauth2_bearer_token(session, jid, password)
+            token_info = await self._oauth2_bearer_token(
+                session, jid, password,
+            )
 
-        http_session[self.SESSION_TOKEN] = token
+        http_session[self.SESSION_TOKEN] = token_info.token
         http_session[self.SESSION_ADDRESS] = jid
+        http_session[self.SESSION_CACHED_SCOPE] = " ".join(token_info.scopes)
         return True
 
     @property
@@ -247,6 +327,30 @@ class ProsodyClient:
                     if not redirect_to:
                         raise abort(401, "Not Authorized")
                     return redirect(url_for(redirect_to))
+
+                return await f(*args, **kwargs)
+            return wrapped
+        return decorator
+
+    def require_admin_session(
+            self,
+            redirect_to: typing.Optional[str] = None,
+            ) -> typing.Callable[
+                [typing.Callable[..., typing.Awaitable[T]]],
+                typing.Callable[..., typing.Awaitable[
+                    typing.Union[T, quart.Response]]]]:
+        def decorator(
+                f: typing.Callable[..., typing.Awaitable[T]],
+                ) -> typing.Callable[..., typing.Awaitable[
+                    typing.Union[T, quart.Response]]]:
+            @functools.wraps(f)
+            @self.require_session(redirect_to=redirect_to)
+            async def wrapped(
+                    *args: typing.Any,
+                    **kwargs: typing.Any,
+                    ) -> typing.Union[T, quart.Response]:
+                if not self.is_admin_session:
+                    raise abort(403, "This is not for you.")
 
                 return await f(*args, **kwargs)
             return wrapped
@@ -309,6 +413,7 @@ class ProsodyClient:
                 "nickname": nickname,
                 "display_name": nickname or localpart,
                 "avatar_hash": avatar_hash,
+                "is_admin": self.is_admin_session,
             }
 
     @autosession
@@ -450,12 +555,115 @@ class ProsodyClient:
             # server to expire/revoke all tokens on password change.
             http_session[self.SESSION_TOKEN] = token
 
+    def _raise_error_from_response(
+            self,
+            resp: aiohttp.ClientResponse,
+            ) -> None:
+        if resp.status in [401, 403]:
+            abort(403, "request rejected by backend")
+        if resp.status == 400:
+            abort(500, "request rejected by backend")
+        if not 200 <= resp.status < 300:
+            abort(resp.status)
+
+    @autosession
+    async def list_users(
+            self,
+            *,
+            session: aiohttp.ClientSession,
+            ) -> typing.Collection[AdminUserInfo]:
+        result = []
+        async with session.get(self._admin_v1_endpoint("/users")) as resp:
+            self._raise_error_from_response(resp)
+            for user in await resp.json():
+                result.append(AdminUserInfo.from_api_response(user))
+        return result
+
+    @autosession
+    async def get_user_by_localpart(
+            self,
+            localpart: str,
+            *,
+            session: aiohttp.ClientSession,
+            ) -> AdminUserInfo:
+        async with session.get(
+                self._admin_v1_endpoint("/users/{}".format(localpart)),
+                ) as resp:
+            self._raise_error_from_response(resp)
+            return AdminUserInfo.from_api_response(await resp.json())
+
+    @autosession
+    async def delete_user_by_localpart(
+            self,
+            localpart: str,
+            *,
+            session: aiohttp.ClientSession,
+            ) -> None:
+        async with session.delete(
+                self._admin_v1_endpoint("/users/{}".format(localpart)),
+                ) as resp:
+            self._raise_error_from_response(resp)
+
+    @autosession
+    async def list_invites(
+            self,
+            *,
+            session: aiohttp.ClientSession,
+            ) -> typing.Collection[AdminInviteInfo]:
+        async with session.get(self._admin_v1_endpoint("/invites")) as resp:
+            self._raise_error_from_response(resp)
+            return list(map(AdminInviteInfo.from_api_response,
+                            await resp.json()))
+
+    @autosession
+    async def get_invite_by_id(
+            self,
+            id_: str,
+            *,
+            session: aiohttp.ClientSession,
+            ) -> AdminInviteInfo:
+        async with session.get(
+                self._admin_v1_endpoint("/invites/{}".format(id_)),
+                ) as resp:
+            self._raise_error_from_response(resp)
+            return AdminInviteInfo.from_api_response(await resp.json())
+
+    @autosession
+    async def delete_invite(
+            self,
+            id_: str,
+            *,
+            session: aiohttp.ClientSession,
+            ) -> None:
+        async with session.delete(
+                self._admin_v1_endpoint("/invites/{}".format(id_)),
+                ) as resp:
+            self._raise_error_from_response(resp)
+
+    @autosession
+    async def create_invite(
+            self,
+            *,
+            session: aiohttp.ClientSession,
+            ) -> AdminInviteInfo:
+        async with session.put(self._admin_v1_endpoint("/invites")) as resp:
+            self._raise_error_from_response(resp)
+            return AdminInviteInfo.from_api_response(await resp.json())
+
     async def logout(self) -> None:
         # this currently only kills the cookie stuff, we may want to invalidate
         # the token on the server side, toos
         # See-Also: https://issues.prosody.im/1503
         http_session.pop(self.SESSION_TOKEN, None)
         http_session.pop(self.SESSION_ADDRESS, None)
+        http_session.pop(self.SESSION_CACHED_SCOPE, None)
+
+    @property
+    def is_admin_session(self) -> bool:
+        if not self.has_session:
+            return False
+        scopes = http_session[self.SESSION_CACHED_SCOPE].split()
+        return SCOPE_ADMIN in scopes
 
 
 client = ProsodyClient()

snikket_web/__init__.py
@@ -17,6 +17,7 @@ import quart.exceptions
 
 from flask_wtf import FlaskForm
 import wtforms
+import flask_babel
 from flask_babel import Babel, _, lazy_gettext as _l
 
 from . import colour, xmpputil
@@ -69,7 +70,7 @@ async def login() -> typing.Union[str, quart.Response]:
         try:
             await client.login(jid, password)
         except quart.exceptions.Unauthorized:
-            form.errors.setdefault("", []).append(
+            form.password.errors.append(
                 _("Invalid user name or password.")
             )
         else:
@@ -166,6 +167,10 @@ def proc() -> typing.Dict[str, typing.Any]:
 
 
 app.template_filter("repr")(repr)
+app.template_filter("format_datetime")(flask_babel.format_datetime)
+app.template_filter("format_date")(flask_babel.format_date)
+app.template_filter("format_time")(flask_babel.format_time)
+app.template_filter("format_timedelta")(flask_babel.format_timedelta)
 
 
 @app.template_filter("flatten")
@@ -175,8 +180,10 @@ def flatten(a: typing.Iterable, levels: int = 1) -> typing.Iterable:
     return a
 
 
-from .user import user_bp  # NOQA
+from .user import user_bp  # noqa:F401,E402
+from .admin import bp as admin_bp  # noqa:F401,E402
 app.register_blueprint(user_bp)
+app.register_blueprint(admin_bp)
 
 logging_config = app.config.get("LOGGING_CONFIG")
 if logging_config is not None:

snikket_web/admin/__init__.py
@@ -1,0 +1,127 @@
+import typing
+
+from datetime import datetime
+
+import quart.flask_patch
+
+import wtforms
+import wtforms.fields.html5
+
+from quart import (Blueprint, render_template, redirect, url_for)
+import flask_wtf
+
+from flask_babel import lazy_gettext as _l
+
+from snikket_web.prosodyclient import client
+
+bp = Blueprint("admin", __name__, url_prefix="/admin")
+
+
+@bp.route("/")
+@client.require_admin_session()
+async def index() -> str:
+    user_info = await client.get_user_info()
+    return await render_template("admin_home.html", user_info=user_info)
+
+
+@bp.route("/users")
+@client.require_admin_session()
+async def users() -> str:
+    user_info = await client.get_user_info()
+    users = sorted(
+        await client.list_users(),
+        key=lambda x: x.localpart
+    )
+    return await render_template(
+        "admin_users.html",
+        users=users,
+        user_info=user_info,
+    )
+
+
+class DeleteUserForm(flask_wtf.FlaskForm):
+    action_delete = wtforms.SubmitField(
+        _l("Delete user permanently")
+    )
+
+
+@bp.route("/user/<localpart>/delete", methods=["GET", "POST"])
+@client.require_admin_session()
+async def delete_user(localpart: str) -> typing.Union[str, quart.Response]:
+    user_info = await client.get_user_info()
+    target_user_info = await client.get_user_by_localpart(localpart)
+    form = DeleteUserForm()
+    if form.validate_on_submit():
+        if form.action_delete.data:
+            await client.delete_user_by_localpart(localpart)
+        return redirect(url_for(".users"))
+
+    return await render_template(
+        "admin_delete_user.html",
+        target_user=target_user_info,
+        user_info=user_info,
+        form=form,
+    )
+
+
+class InvitesListForm(flask_wtf.FlaskForm):
+    action_revoke = wtforms.StringField()
+
+    action_create_invite = wtforms.SubmitField(
+        _l("New invitation link")
+    )
+
+
+@bp.route("/invitations", methods=["GET", "POST"])
+@client.require_admin_session()
+async def invitations() -> typing.Union[str, quart.Response]:
+    user_info = await client.get_user_info()
+    invites = sorted(
+        await client.list_invites(),
+        key=lambda x: x.created_at
+    )
+
+    form = InvitesListForm()
+    if form.validate_on_submit():
+        if form.action_revoke.data:
+            await client.delete_invite(form.action_revoke.data)
+        if form.action_create_invite.data:
+            info = await client.create_invite()
+            return redirect(url_for(".edit_invite", id_=info.id_))
+        return redirect(url_for(".invitations"))
+
+    return await render_template(
+        "admin_invites.html",
+        user_info=user_info,
+        invites=invites,
+        now=datetime.utcnow(),
+        form=form,
+    )
+
+
+class InviteForm(flask_wtf.FlaskForm):
+    action_revoke = wtforms.SubmitField(
+        _l("Revoke")
+    )
+
+
+@bp.route("/invitation/<id_>", methods=["GET", "POST"])
+@client.require_admin_session()
+async def edit_invite(id_: str) -> typing.Union[str, quart.Response]:
+    user_info = await client.get_user_info()
+    invite_info = await client.get_invite_by_id(id_)
+
+    form = InviteForm()
+    if form.validate_on_submit():
+        if form.action_revoke.data:
+            await client.delete_invite(id_)
+            return redirect(url_for(".invitations"))
+        return redirect(url_for(".edit_invite", id_=id_))
+
+    return await render_template(
+        "admin_edit_invite.html",
+        user_info=user_info,
+        invite=invite_info,
+        now=datetime.utcnow(),
+        form=form,
+    )

snikket_web/scss/_baseline.scss
@@ -7,7 +7,7 @@ body {
 	color: $gray-100;
 }
 
-p, blockquote, ul, ol {
+p, blockquote, ul, ol, table, dl {
 	line-height: 1.5;
 	margin: 1.5em 0;
 	font-family: $font-bulk;
@@ -17,6 +17,10 @@ p, blockquote, ul, ol {
 blockquote {
 	margin-left: $w-l2;
 	margin-right: $w-l2;
+}
+
+dt {
+	font-weight: bold;
 }
 
 h1, h2, h3, h4, h5, h6 {

snikket_web/scss/_theme.scss
@@ -59,7 +59,7 @@ $colours: (
 $box-types: (
 	"primary": "blue",
 	"accent": "yellow",
-	"alert": "alert",
+	"alert": "red",
 	"warning": "yellow",
 	"success": "green",
 	"hint": "blue"

snikket_web/scss/app.scss
@@ -186,7 +186,7 @@ p.form-desc.weak {
 	color: $gray-300;
 }
 
-$text-entry-inputs: "text" "password";
+$text-entry-inputs: "text" "password" "email" "tel";
 
 div.f-errbox {
 	background-color: $alert-800;
@@ -390,9 +390,30 @@ div.form.layout-expanded {
 
 /* form buttons */
 
-button, .button {
+.btn-edit:before {
+	content: '✎';
+}
+
+.btn-delete:before {
+	content: '🗑';
+}
+
+.btn-link:before {
+	content: '+';
+}
+
+.btn-more:before {
+	content: '…';
+}
+
+input[type="submit"], button, .button {
 	margin: 0 $w-s2;
 	padding: $w-s3 $w-s1;
+
+	td & {
+		margin: 0 $w-s4;
+		padding: $w-s4 $w-s2;
+	}
 }
 
 a.button {
@@ -400,141 +421,145 @@ a.button {
 	cursor: default;
 }
 
-button.primary, .button.primary {
-	background: linear-gradient(0deg, $primary-500, $primary-600);
-	box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
-	color: $primary-900;
-	border: none;
-	/* TODO: fix vertical rhyhtm ... */
-	border-radius: $w-s4;
-	// border: $w-s4 solid transparent;
-
-	&:hover, &:focus {
-		background: linear-gradient(0deg, $primary-600, $primary-700);
-		color: white;
-	}
-
-	&:active {
-		background: $primary-500;
-		box-shadow: inset 0px 2px 4px rgba(0, 0, 0, 0.2);
-		color: white;
-	}
-
-	&.accent {
-		background: linear-gradient(0deg, $accent-500, $accent-600);
-		color: $accent-900;
+input[type="submit"], button, .button {
+	&.primary {
+		background: linear-gradient(0deg, $primary-500, $primary-600);
+		box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
+		color: $primary-900;
+		border: none;
+		/* TODO: fix vertical rhyhtm ... */
+		border-radius: $w-s4;
+		// border: $w-s4 solid transparent;
 
 		&:hover, &:focus {
+			background: linear-gradient(0deg, $primary-600, $primary-700);
+			color: white;
+		}
+
+		&:active {
+			background: $primary-500;
+			box-shadow: inset 0px 2px 4px rgba(0, 0, 0, 0.2);
+			color: white;
+		}
+
+		&.accent {
+			background: linear-gradient(0deg, $accent-500, $accent-600);
+			color: $accent-900;
+
+			&:hover, &:focus {
+				background: linear-gradient(0deg, $accent-600, $accent-700);
+			}
+
+			&:active {
+				background: $accent-500;
+			}
+		}
+
+		&.danger {
+			background: linear-gradient(0deg, $alert-500, $alert-600);
+			color: $alert-900;
+
+			&:hover, &:focus {
+				background: linear-gradient(0deg, $alert-600, $alert-700);
+			}
+
+			&:active {
+				background: $alert-500;
+			}
+		}
+	}
+
+	&.secondary {
+		background: linear-gradient(0deg, $gray-600, $gray-700);
+		box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
+		color: $gray-200;
+		border: none;
+		/* TODO: fix vertical rhyhtm ... */
+		border-radius: $w-s4;
+		// border: $w-s4 solid transparent;
+
+		&:hover, &:focus {
+			background: linear-gradient(0deg, $gray-700, $gray-800);
+			color: black;
+		}
+
+		&:active {
+			background: $gray-600;
+			box-shadow: inset 0px 2px 4px rgba(0, 0, 0, 0.2);
+			color: black;
+		}
+
+		&.accent {
 			background: linear-gradient(0deg, $accent-600, $accent-700);
+			color: $accent-200;
+
+			&:hover, &:focus {
+				background: linear-gradient(0deg, $accent-700, $accent-800);
+			}
+
+			&:active {
+				background: $accent-600;
+			}
 		}
 
-		&:active {
-			background: $accent-500;
-		}
-	}
-
-	&.danger {
-		background: linear-gradient(0deg, $alert-500, $alert-600);
-		color: $alert-900;
-
-		&:hover, &:focus {
+		&.danger {
 			background: linear-gradient(0deg, $alert-600, $alert-700);
-		}
+			color: $alert-200;
 
-		&:active {
-			background: $alert-500;
-		}
-	}
-}
+			&:hover, &:focus {
+				background: linear-gradient(0deg, $alert-700, $alert-800);
+			}
 
-button.secondary, .button.secondary {
-	background: linear-gradient(0deg, $gray-600, $gray-700);
-	box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
-	color: $gray-200;
-	border: none;
-	/* TODO: fix vertical rhyhtm ... */
-	border-radius: $w-s4;
-	// border: $w-s4 solid transparent;
-
-	&:hover, &:focus {
-		background: linear-gradient(0deg, $gray-700, $gray-800);
-		color: black;
-	}
-
-	&:active {
-		background: $gray-600;
-		box-shadow: inset 0px 2px 4px rgba(0, 0, 0, 0.2);
-		color: black;
-	}
-
-	&.accent {
-		background: linear-gradient(0deg, $accent-600, $accent-700);
-		color: $accent-200;
-
-		&:hover, &:focus {
-			background: linear-gradient(0deg, $accent-700, $accent-800);
-		}
-
-		&:active {
-			background: $accent-600;
+			&:active {
+				background: $alert-600;
+			}
 		}
 	}
 
-	&.danger {
-		background: linear-gradient(0deg, $alert-600, $alert-700);
-		color: $alert-200;
-
-		&:hover, &:focus {
-			background: linear-gradient(0deg, $alert-700, $alert-800);
-		}
-
-		&:active {
-			background: $alert-600;
-		}
-	}
-}
-
-button.tertiary, .button.tertiary {
-	background-color: transparent;
-	color: $gray-100;
-	border: none;
-	text-decoration: underline;
-	/* TODO: fix vertical rhyhtm ... */
-	border-radius: $w-s4;
-
-	&:hover {
-		background-color: $gray-900;
-		border-color: $gray-800;
-		color: black;
-	}
-
-	&.accent {
-		text-decoration-color: $accent-500;
+	&.tertiary, .tertiary {
+		background-color: transparent;
+		color: $gray-100;
+		border: none;
+		text-decoration: underline;
+		/* TODO: fix vertical rhyhtm ... */
+		border-radius: $w-s4;
 
 		&:hover {
-			background-color: $accent-900;
-			border-color: $accent-800;
+			background-color: $gray-900;
+			border-color: $gray-800;
 			color: black;
+		}
+
+		&.accent {
+			text-decoration-color: $accent-500;
+
+			&:hover {
+				background-color: $accent-900;
+				border-color: $accent-800;
+				color: black;
+			}
+		}
+
+		&.danger {
+			text-decoration-color: $alert-500;
+
+			&:hover {
+				background-color: $alert-900;
+				border-color: $alert-800;
+				color: black;
+			}
 		}
 	}
 
-	&.danger {
-		text-decoration-color: $alert-500;
-
-		&:hover {
-			background-color: $alert-900;
-			border-color: $alert-800;
-			color: black;
-		}
+	&.fullwidth {
+		display: block;
+		width: 100%;
+		margin-left: 0;
+		margin-right: 0;
 	}
 }
 
-button.fullwidth, .button.fullwidth {
-	display: block;
-	width: 100%;
-	margin-left: 0;
-	margin-right: 0;
-}
+
 
 /* button, .button {
 	margin: 0 $w-s2;
@@ -710,7 +735,7 @@ div.welcome-cards {
 	flex-wrap: wrap;
 
 	& > .card {
-		flex: 1 0 $w-l6;
+		flex: 1 0 $w-l7;
 		margin: $w-s1;
 		@extend .el-2;
 		padding: $w-s1 $w-l1;
@@ -733,6 +758,24 @@ div.welcome-cards {
 }
 
 
+/* admin area specials */
+
+#topbar > div.admin-note {
+	color: $alert-500;
+	font-size: nth($h-sizes, 5);
+	margin-left: $w-l1;
+}
+
+table {
+	border-collapse: collapse;
+	width: 100%;
+
+	td, th {
+		padding: $w-s1;
+	}
+}
+
+
 /* linearisation / responsive stuff */
 
 @media screen and (max-width: $small-screen-threshold) {
@@ -751,4 +794,31 @@ div.welcome-cards {
 	}
 
 
+	th.collapsible, td.collapsible {
+		display: none;
+	}
+
+	#topbar.admin {
+		> header {
+			text-decoration: underline;
+			text-decoration-color: $alert-500;
+		}
+
+		> div.admin-note {
+			display: none;
+		}
+	}
+}
+
+/* clipboard button */
+
+.copy-to-clipboard {
+	cursor: pointer;
+	margin-left: 0.5em;
+	font-style: normal;
+	text-decoration: none;
+}
+
+body.no-copy .copy-to-clipboard {
+	display: none !important;
 }

snikket_web/templates/admin_app.html
@@ -1,0 +1,6 @@
+{% extends "app.html" %}
+{% block topbar_classes %}{{ super() }} admin{% endblock %}
+{% block topbar_left %}
+{{ super() }}
+<div class="admin-note">Admin area</div>
+{% endblock %}

snikket_web/templates/admin_delete_user.html
@@ -1,0 +1,27 @@
+{% extends "admin_app.html" %}
+{% from "library.j2" import box %}
+{% block content %}
+<h1>{% trans user_name=target_user.localpart %}Delete user {{ user_name }}{% endtrans %}</h1>
+<div class="form layout-expanded"><form method="POST">
+	<h2 class="form-title">{% trans %}Delete user{% endtrans %}</h2>
+	{{ form.csrf_token }}
+	<p class="form-descr">{% trans %}Are you sure you want to delete the following user?{% endtrans %}</p>
+	<dl>
+		<dt>{% trans %}Login name{% endtrans %}</dt>
+		<dd>{{ target_user.localpart }}</dd>
+		<dt>{% trans %}Display name{% endtrans %}</dt>
+		<dd>{{ target_user.display_name }}</dd>
+		<dt>{% trans %}Email address{% endtrans %}</dt>
+		<dd>{{ target_user.email }}</dd>
+		<dt>{% trans %}Display name{% endtrans %}</dt>
+		<dd>{{ target_user.phone }}</dd>
+	</dl>
+	{% call box("alert", _("Danger")) %}
+	<p>The user and their data will be deleted irrevocably, permanently and immediately upon pushing thre below button. <strong>There is no way back!</strong></p>
+	{% endcall %}
+	<div class="f-bbox">
+		<a class="button secondary" href="{{ url_for('.users') }}">Back</a>
+		{{ form.action_delete(class="primary danger") }}
+	</div>
+</form></div>
+{% endblock %}

commit 1713da61e79e36248cd8b9f9fdbb6e26a1baa3ce

Fix password change
This fixes a regression introduced in e476d9b7 which caused the
token to be incorrectly used when authenticating with mod_rest for
sending the password change IQ stanza.

snikket_web/prosodyclient.py
@@ -332,15 +332,18 @@ class ProsodyClient:
                 )
             )
 
+    def _store_token_in_session(self, token_info: TokenInfo):
+        http_session[self.SESSION_TOKEN] = token_info.token
+        http_session[self.SESSION_CACHED_SCOPE] = " ".join(token_info.scopes)
+
     async def login(self, jid: str, password: str) -> bool:
         async with self._plain_session as session:
             token_info = await self._oauth2_bearer_token(
                 session, jid, password,
             )
 
-        http_session[self.SESSION_TOKEN] = token_info.token
+        self._store_token_in_session(token_info)
         http_session[self.SESSION_ADDRESS] = jid
-        http_session[self.SESSION_CACHED_SCOPE] = " ".join(token_info.scopes)
         return True
 
     @property
@@ -767,7 +770,7 @@ class ProsodyClient:
         # got there, replacing the current session token on the way.
 
         async with self._plain_session as session:
-            token = await self._oauth2_bearer_token(
+            token_info = await self._oauth2_bearer_token(
                 session,
                 self.session_address,
                 current_password,
@@ -779,14 +782,14 @@ class ProsodyClient:
                     new_password
                 ),
                 headers={
-                    "Authorization": "Bearer {}".format(token),
+                    "Authorization": "Bearer {}".format(token_info.token),
                 },
                 sensitive=True,
             )
             # TODO: error handling
             # TODO: obtain a new token using the new password to allow the
             # server to expire/revoke all tokens on password change.
-            http_session[self.SESSION_TOKEN] = token
+            self._store_token_in_session(token_info)
 
     def _raise_error_from_response(
             self,