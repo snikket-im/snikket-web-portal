snikket-im/snikket-web-portal

commit 5d7183a0b8e551f52206b4573a5b4a6c042eb071

Reinstate cache deletion in multi-stage build
Previously, the multi-stage build increased the image size by about 30
MiB (163MiB -> 191MiB). Dropping the caches reduces the image size down
to 159MiB, leading to a net improvement of 4 MiB.

Dockerfile
@@ -40,12 +40,14 @@ RUN set -eu; \
     apt-get update ; \
     apt-get install -y --no-install-recommends \
         python3 python3-pip python3-setuptools python3-wheel; \
-    pip3 install hypercorn;
+    apt-get clean ; rm -rf /var/lib/apt/lists; \
+    pip3 install hypercorn; \
+    rm -rf /root/.cache;
 
 WORKDIR /opt/snikket-web-portal
 
 COPY requirements.txt /opt/snikket-web-portal/requirements.txt
-RUN pip3 install -r requirements.txt;
+RUN pip3 install -r requirements.txt; rm -rf /root/.cache;
 
 COPY --from=build /opt/snikket-web-portal/snikket_web/ /opt/snikket-web-portal/snikket_web
 COPY babel.cfg /opt/snikket-web-portal/babel.cfg

commit 6aa6e126808c7c1ee31484e182010b95ed01deca

Fix error handling when building image
Apparently, we managed to publish an image without working aiohttp
because of this.

Partially a regression from 5d7183a.

Dockerfile
@@ -17,7 +17,8 @@ COPY babel.cfg /opt/snikket-web-portal/babel.cfg
 
 WORKDIR /opt/snikket-web-portal
 
-RUN pip3 install -r requirements.txt; \
+RUN set -eu; \
+    pip3 install -r requirements.txt; \
     pip3 install -r build-requirements.txt; \
     make;
 
@@ -47,7 +48,7 @@ RUN set -eu; \
 WORKDIR /opt/snikket-web-portal
 
 COPY requirements.txt /opt/snikket-web-portal/requirements.txt
-RUN pip3 install -r requirements.txt; rm -rf /root/.cache;
+RUN set -eu; pip3 install -r requirements.txt; rm -rf /root/.cache;
 
 COPY --from=build /opt/snikket-web-portal/snikket_web/ /opt/snikket-web-portal/snikket_web
 COPY babel.cfg /opt/snikket-web-portal/babel.cfg